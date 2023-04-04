8e796fec1783e583869eb6fe566078bf8651a0fe
David J <98175896+jiangd22@users.noreply.github.com>
2023-04-04 05:42:27 -0500

    merging stashes with current branch

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,12 @@
-<<<<<<< Updated upstream
-=======
 import { HomePage } from "./webpages";
 import { Profile } from "./webpages/profile";
 
->>>>>>> Stashed changes
 export const App = () => {
 
   return (
     <>
-<<<<<<< Updated upstream
-    <h1>My branch</h1>
-    <h2>Testing git push to my branch</h2>
-=======
     {/* <HomePage /> */}
     <Profile />
->>>>>>> Stashed changes
     </>
   );
 }
