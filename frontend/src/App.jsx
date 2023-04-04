76aa58cd6af37eaecdcb8568552d00923f714a27
David J <98175896+jiangd22@users.noreply.github.com>
2023-04-04 06:42:24 -0500

    navBar, profile work saved

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,14 @@
 import { HomePage } from "./webpages";
 import { Profile } from "./webpages/profile";
+import * as React from "react";
+import { ChakraProvider } from "@chakra-ui/react";
 
 export const App = () => {
 
   return (
     <>
-    {/* <HomePage /> */}
-    <Profile />
+    <HomePage />
+    {/* <Profile /> */}
     </>
   );
 }
