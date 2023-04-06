93849a6e795eef0b2f841da198583e93b8efaed8
Rofo <88415585+rudycito23@users.noreply.github.com>
2023-04-06 13:21:13 -0500

    Added navigation to dashboard after creating a new user.

--- frontend/src/components/JoinModal.jsx
+++ frontend/src/components/JoinModal.jsx
@@ -16,9 +16,9 @@ import {
   Box,
   ButtonGroup,
   Text,
-  Link
+  Link,
 } from "@chakra-ui/react";
-import { useRouter } from "react-router-dom";
+import { useNavigate, useRouter } from "react-router-dom";
 import { useState, useRef } from "react";
 
 export const JoinModal = () => {
@@ -30,14 +30,20 @@ export const JoinModal = () => {
   const [newUserPassword, setNewUserPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
 
-  const handleNewUserFirstName = (e) => setNewUserFirstName(e.target.value);
-  const handleNewUserLastName = (e) => setNewUserLastName(e.target.value);
+  const handleNewUserFirstName = (e) =>
+    setNewUserFirstName(e.target.value);
+  const handleNewUserLastName = (e) =>
+    setNewUserLastName(e.target.value);
   const handleNewUserEmail = (e) => setNewUserEmail(e.target.value);
-  const handleNewUserPassword = (e) => setNewUserPassword(e.target.value);
+  const handleNewUserPassword = (e) =>
+    setNewUserPassword(e.target.value);
   // const handleConfirmPassword = (e) => setConfirmPassword(e.target.value);
 
-  const url = 'http://localhost:8000';
+  const navigate = useNavigate();
 
+  const url = "http://localhost:8000";
+
+  // check if we are connecting
   const checkAPI = () => {
     axios
       .get(url + "/")
@@ -49,46 +55,62 @@ export const JoinModal = () => {
       });
   };
 
+  // assign firstName, lastName, email, and password to user
   const user = {
-    "firstName": newUserFirstName,
-    "lastName": newUserLastName,
-    "email": newUserEmail,
-    "password": newUserPassword
-  }
+    firstName: newUserFirstName,
+    lastName: newUserLastName,
+    email: newUserEmail,
+    password: newUserPassword,
+  };
 
-const sendJSON = () => {
-  console.log(user);
+  // package all the data in user as a JSON
+  const sendJSON = () => {
+    console.log(user);
+    axios
+      .put(url + "/parse", user)
+      .then((response) => {
+        alert(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
 
-  axios.put(url + '/parse', user).then((response) => {
-    alert(response.data)
-  }).catch((error) => {
-    console.log(error)
-  })
-}  
+  // send and post the new user data to the backend and table,
+  // then, navigate the new user to their dashboard
+  const sendNewUser = () => {
+    axios
+      .post(url + "/user", user)
+      .then((response) => {
+        // alert(response.data)
+        navigate("/home");
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
 
-const sendNewUser = () => {
-  axios.post(url + '/user', user).then((response) => {
-    alert(response.data)
-  }).catch((error) => {
-    console.log(error)
-  })
-}
+  const getNewUsers = () => {
+    axios
+      .get(url + "/users")
+      .then((response) => {
+        alert(JSON.stringify(response.data));
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
 
-const getNewUsers = () => {
-  axios.get(url + '/users').then((response) => {
-    alert(JSON.stringify(response.data))
-  }).catch((error) => {
-    console.log(error)
-  })
-}
-
-const clearUsers = () => {
-  axios.put(url + '/users/clear').then((response) => {
-    alert(response.data)
-  }).catch((error) => {
-    console.log(error)
-  })
-}
+  const clearUsers = () => {
+    axios
+      .put(url + "/users/clear")
+      .then((response) => {
+        alert(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
 
   return (
     <>
@@ -101,28 +123,64 @@ const clearUsers = () => {
           <ModalHeader>Create an account</ModalHeader>
           <ModalCloseButton />
           <ModalBody>
-          <FormControl isRequired>
+            <FormControl isRequired>
               <FormLabel>First name:</FormLabel>
-              <Input type="text" placeHolder="First name" onChange={handleNewUserFirstName}/>
+              <Input
+                type="text"
+                placeHolder="First name"
+                onChange={handleNewUserFirstName}
+              />
               <FormLabel mt=".5rem">Last name:</FormLabel>
-              <Input type="text" placeHolder="Last name" onChange={handleNewUserLastName}/>
+              <Input
+                type="text"
+                placeHolder="Last name"
+                onChange={handleNewUserLastName}
+              />
               <FormLabel mt=".5rem">Email:</FormLabel>
-              <Input type="email" placeHolder="name@email.com" onChange={handleNewUserEmail}/>
+              <Input
+                type="email"
+                placeHolder="name@email.com"
+                onChange={handleNewUserEmail}
+              />
               <FormLabel mt=".5rem">Create password:</FormLabel>
-              <Input type="text" placeHolder="" onChange={handleNewUserPassword}/>
+              <Input
+                type="text"
+                placeHolder=""
+                onChange={handleNewUserPassword}
+              />
               {/* <FormLabel mt=".5rem">Confirm password:</FormLabel>
               <Input type="text" placeHolder="" onChange={handleConfirmPassword}/> */}
             </FormControl>
           </ModalBody>
           <ModalFooter>
-          <ButtonGroup gap='2'>
-            <Button color='white' bg="#0C97FA" variant='outline' _hover={{ bg: "white", color: "#0C97FA", border: '2px'}} onClick={sendNewUser}>Create</Button>
-            <Button onClick={onClose} _hover={{ bg: "white", color: "#FF176B", border: '2px'}}>Close</Button>
+            <ButtonGroup gap="2">
+              <Button
+                color="white"
+                bg="#0C97FA"
+                variant="outline"
+                _hover={{
+                  bg: "white",
+                  color: "#0C97FA",
+                  border: "2px",
+                }}
+                onClick={sendNewUser}
+              >
+                Create
+              </Button>
+              <Button
+                onClick={onClose}
+                _hover={{
+                  bg: "white",
+                  color: "#FF176B",
+                  border: "2px",
+                }}
+              >
+                Close
+              </Button>
             </ButtonGroup>
           </ModalFooter>
         </ModalContent>
       </Modal>
-    
     </>
   );
 };
